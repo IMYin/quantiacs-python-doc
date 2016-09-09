406ef16c8e3e06f3356f3122573b93c7ece5e59c
YalaTest <yaleis@andrew.cmu.edu>
2016-09-08 17:43:39 -0700

    add toolbox to names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -285,7 +285,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'QuantiacsPython.tex', u'Quantiacs Python Toolbox Documentation',
+    (master_doc, 'QuantiacsPythonToolbox.tex', u'Quantiacs Python Toolbox Documentation',
      u'Quantiacs', 'manual'),
 ]
 
@@ -327,7 +327,7 @@ latex_logo = "quantiacs-logo.png"
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'quantiacspython', u'Quantiacs Python Toolbox Documentation',
+    (master_doc, 'quantiacspythonToolbox', u'Quantiacs Python Toolbox Documentation',
      [author], 1)
 ]
 
@@ -342,8 +342,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'QuantiacsPython', u'Quantiacs Python Toolbox Documentation',
-     author, 'QuantiacsPython', 'One line description of project.',
+    (master_doc, 'quantiacsPythonToolbox', u'Quantiacs Python Toolbox Documentation',
+     author, 'quantiacsPythonToolbox', 'One line description of project.',
      'Miscellaneous'),
 ]
 
